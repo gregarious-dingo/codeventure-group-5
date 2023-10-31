451e99aa651685b6b61ada66fcacb69449ad15c7
vincentanu04 <vincentanu04@gmail.com>
2023-11-01 03:29:53 +0800

    slight update

--- challenge_frame.py
+++ challenge_frame.py
@@ -40,10 +40,7 @@ class ChallengeFrame(tk.Frame):
         next_challenge_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
     def submit(self):
-        answer=self.answer_text.get('1.0', 'end-1c')
-        print(answer)
-        print(self.current_challenge['correct_answer'])
-        print(answer == self.current_challenge['correct_answer'])
+        answer=self.answer_text.get('1.0', 'end').strip()
         if answer == self.current_challenge['correct_answer']:
             self.challenge_score += 1
 
